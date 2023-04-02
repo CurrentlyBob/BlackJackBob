02d668a408d2dd37b9cdaa87b6d09d2c17227511
BobWa <walshbobby5@gmail.com>
2023-04-02 17:58:55 -0500

    declare variables, add render function

---
+++ app.js
@@ -0,0 +1,26 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let dealerSum = 0 
+let userSum = 0
+let dealerAceCount = 0
+let userAceCount = 0
+let secret, deck
+
+let hit = true //can hit if sum is less than 21
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+function render() {
+    buildDeck();
+}
